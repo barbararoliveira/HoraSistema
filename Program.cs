7f48aa5e54519a30c0cb2b3bd7e47bce38b898e1
Bárbara <babyr.oliveira@gmail.com>
2020-06-01 16:28:31 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.White; 
+           Console.WriteLine("Pressione enter para saber a data e a hora atual:");
+
+           Console.ReadKey();
+
+           DateTime DataDoSistema = DateTime.Now;
+
+           Console.Write("Neste momento são exatamente: ");
+           Console.ForegroundColor = ConsoleColor.Magenta; 
+           Console.WriteLine (DataDoSistema);
+        }
+    }
+}
